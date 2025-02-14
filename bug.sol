989aee8007665885bd3fbfdd83023c1bfd011eb0
BugHunter-X <askzenai@gmail.com>
2025-02-14 19:20:06 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,9 @@
+```solidity
+function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+}
+```
